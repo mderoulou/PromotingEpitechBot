6ddbb2cb5fe1aaed76e5070e459d4e5436e79923
Maxime Deroulou <maxime.deroulou@epitech.eu>
2021-02-18 17:13:58 +0100

    [+] Add project on git

---
+++ collector.py
@@ -0,0 +1,125 @@
+#!/bin/python3
+import pymysql
+import discord
+from discord.ext import commands
+import sys as s
+import os as o
+from minesql import create_con
+
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+client = commands.Bot(command_prefix='>', help_command=None, intents=intents)
+current_guild = 691661291785551873
+current_focus = int(s.argv[1])
+step = 0
+guild = None
+member = None
+pid = o.getpid()
+text = []
+
+async def close_bot(comment=None):
+    con = create_con()
+    try:
+        with con.cursor() as c:
+            c.execute("UPDATE EpiCom.members SET pid_collector=NULL, comment = %s WHERE (uid=%s);", (comment,current_focus))
+        con.commit()
+    finally:
+        con.close()
+    if (comment == None):
+        print(f"Collecting data of {member} ({member.id}) - done.")
+    await client.close()
+
+async def send_a_message(message):
+    try:
+        await member.send(message)
+    except:
+        print(f"DM are disabled by : {member} ({member.id})")
+        await close_bot("DM are disabled !")
+
+@client.event
+async def on_command_error(ctx, error):
+    pass
+
+@client.event
+async def on_ready():
+    global guild, member, step, text
+    guild = client.get_guild(current_guild)
+    member = guild.get_member(current_focus)
+    user = await client.fetch_user(current_focus)
+    if (member == None):
+        print(f"No member found with corresponding id ! ({current_focus})")
+        await client.close()
+        return
+    print(f"Collecting data for : {member} ({member.id})")
+    con = create_con()
+    try:
+        with con.cursor() as c:
+            c.execute("SELECT step FROM EpiCom.members WHERE (uid=%s);", (current_focus))
+            res = c.fetchone()
+            if (res == None):
+                c.execute("INSERT INTO EpiCom.members (uid, pid_collector) VALUES (%s, %s);", (current_focus, pid))
+                con.commit()
+                step = 0
+            else:
+                step = res[0]
+    finally:
+        con.close()
+    if (step == 2):
+        await close_bot()
+        return
+    text = [
+            f":wave: Salut {member.mention} !\nBienvenue sur le discord d'EPITECH Lille !\nJ'aurai besoin que tu me donnes ton prénom et nom de famille :smiley:",
+            ":upside_down: Pourrais-tu nous indiquer, les étude(s) et spécialité(s) et le lycéen dans lesquelles tu te situe actuellement ?",
+            "Merci pour ces informations. :+1:\n:information_source: Sophie Epitech ou Fabienne Lély va prochainement te contacter !\n:grey_question: N'hésites pas à nous poser des questions dans le salon question du discord !\nNote: Tes informations ne seront divulguées à personnes d'autre que l'administration d'EPITECH Lille, sois-en sûr !\n"
+           ]
+    await send_a_message(text[step])
+
+@client.event
+async def on_message(message):
+    global step
+    if (message.author == client.user or message.author.id != member.id or message.guild != None):
+        return
+    if (message.content == "close"):
+        await message.author.send("Um you're a member of EPITECH family, okay ...")
+        con = create_con()
+        try:
+            with con.cursor() as c:
+                c.execute("DELETE FROM EpiCom.members WHERE (uid=%s);", (current_focus))
+            con.commit()
+        finally:
+            con.close()
+        await close_bot()
+        return
+    if (step == 0):
+        con = create_con()
+        step = 1
+        try:
+            with con.cursor() as c:
+                c.execute("UPDATE EpiCom.members SET step = %s, fullname = %s WHERE (uid=%s);", (step, message.content, current_focus))
+            con.commit()
+        finally:
+            con.close()
+        try:
+            await member.edit(nick=message.content)
+        except:
+            pass
+        await message.author.send(text[step])
+        return
+    if (step == 1):
+        con = create_con()
+        step = 2
+        try:
+            with con.cursor() as c:
+                c.execute("UPDATE EpiCom.members SET step = %s, studies = %s WHERE (uid=%s);", (step, message.content, current_focus))
+            con.commit()
+        finally:
+            con.close()
+        await message.author.send(text[step])
+        await close_bot()
+        return
+
+def read_token():
+    with open("token.txt", "r") as f:
+        return (f.readlines()[0].strip())
+client.run(read_token())
---
+++ com_bot.py
@@ -0,0 +1,277 @@
+#!/bin/python3
+import pymysql
+import discord
+from discord.ext import commands
+import re
+import os
+import json
+import datetime
+import xlsxwriter
+import subprocess
+from minesql import create_con
+import random
+
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+client = commands.Bot(command_prefix='>', help_command=None, intents=intents)
+with open("welcome.txt", "r") as f:
+    welcome_channel = int(f.read())
+welcome_text = ["**➡️** On souhaite la bienvenue à {0} sur le discord d'Epitech ! :tada:",
+                "**➡️** {0} a rush B, puis est arrivé parmis nous, on te souhaite la bienvenue ! :boom:",
+                "**➡️** Erreur 404 ... Ah non code 200 je voulais dire, bienvenue {0} ! :wrench:",
+                "**➡️** {0} est arrivé ! Tous à vos téléphones ! :mobile_phone:",
+                "**➡️** {0} est venu pour taper du code ! Bienvenue à toi ! :bulb:",
+                "**➡️** Un {0} sauvage apparaît ! Bienvenue à toi ! :sparkles:",
+                "**➡️** Code de triche activé ! {0} arrive sur le serveur ! :joystick:",
+                "**➡️** Rentre vite {0}, il fait froid dehors ! :fire:",
+                "**➡️** Bonne nouvelle ! {0} est arrivé à destination ! :gift:",
+                "**➡️** {0} \* boup bip bou \* Bienvenue ! :robot:",
+                "**➡️** Toi tu vis, toi tu vis, toi tu arrives ! Bienvenue {0} ! :luggage:"]
+
+def make_embed(title="", description="", nb_field=0, fields={}, inline=False, color=3):
+    colors = [0xbd0f0f, 0x3700ff, 0x0ab007, 0x640066] # Red / Info / Green / others
+    embed=discord.Embed(description=description,color=colors[color])
+    if (nb_field):
+        for x in fields:
+            embed.add_field(name=x, value=fields[x], inline=inline)
+    embed.set_footer(text="Dewey bot, by Maxime D.", icon_url="https://cdn.discordapp.com/icons/691661291785551873/f5fd1028da30dc55eb06ea77f6dbf222.png?size=128")
+    if (color == 0):
+        url = "https://i.ibb.co/tM7pYZr/error.png"
+    elif (color == 1):
+        url = "https://i.ibb.co/sqfPRNC/info.png"
+    elif (color == 2):
+        url = "https://i.ibb.co/qDQ8dwM/success.png"
+    else:
+        url = ""
+    embed.set_author(name=title, icon_url=url)
+    return embed
+
+@client.event
+async def on_ready():
+    print("Logged as {0.user}".format(client))
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Malcolm"))
+
+@client.event
+async def on_member_join(member):
+    d = datetime.datetime.today()
+    dstr = "{:02d}".format(d.month) + "-" + str(d.year)
+    channel = client.get_channel(welcome_channel)
+    await channel.send(random.choice(welcome_text).format(member.mention))
+    try:
+        con = create_con()
+        with con.cursor() as c:
+            c.execute("SELECT uid FROM EpiCom.members WHERE (uid = %s);", member.id)
+            if (c.fetchone() == None):
+                print(f"Collecting data for : {member} ({member.id})")
+                subprocess.Popen(["python3", "collector.py", str(member.id)], shell=False)
+            c.execute("SELECT nb_join FROM EpiCom.stats WHERE (month = %s);", dstr)
+            res = c.fetchone()
+            if (res == None):
+                c.execute("INSERT INTO EpiCom.stats (month, nb_join) VALUES (%s, %s);", (dstr, 1))
+                con.commit()
+            else:
+                c.execute("UPDATE EpiCom.stats SET nb_join = %s WHERE (month = %s);", (res[0] + 1, dstr))
+                con.commit()
+            c.execute("DELETE FROM EpiCom.members WHERE (insert_date < %s )", (str(datetime.datetime.now().year-1) + "-" + str(datetime.datetime.now().month) + "-" + str(datetime.datetime.now().day)))
+            con.commit()
+    except:
+        pass
+    finally:
+        con.close()
+
+@client.command(name='export')
+@commands.guild_only()
+async def _export(ctx, role: str, coma=","):
+    print("{0} used export cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976 or ("staff epitech" in [z.name.lower() for z in ctx.message.author.roles])):
+        i = 0
+        name = "export_" + role + "_" + str(datetime.datetime.today().month)
+        with open(name + ".csv", "w") as f:
+            workbook = xlsxwriter.Workbook(name + ".xlsx")
+            worksheet = workbook.add_worksheet()
+            con = create_con()
+            try:
+                array = ["Numéro", "Identifiant", "Nom d'affichage", "UID", "Etudes", "Comment"]
+                for x in range(0, len(array)):
+                    worksheet.write(i, x, array[x])
+                f.write("n°" + coma + "Identifiant" + coma + "Nom d'affichage" + coma + "UID" + coma + "Etudes" + coma + "Comment\r\n")
+                with con.cursor() as c:
+                    async for x in ctx.message.guild.fetch_members(limit=None):
+                        if (role.lower() in [y.name.lower() for y in x.roles]):
+                            c.execute("SELECT studies,comment FROM EpiCom.members WHERE (uid = %s);", (str(x.id)))
+                            res = c.fetchone()
+                            i += 1
+                            if (res == None):
+                                f.write(str(i) + coma + str(x) + coma + str(x.display_name)+ coma + str(x.id) + "\r\n" )
+                                array = [i, str(x), str(x.display_name), str(x.id)]
+                                for z in range(0, len(array)):
+                                    worksheet.write(i, z, array[z])
+                            else:
+                                f.write(str(i) + coma + str(x) + coma + str(x.display_name)+ coma + str(x.id) + coma + str(res[0]) + coma + str(res[1]) + "\r\n" )
+                                array = [i, str(x), str(x.display_name), str(x.id), str(res[0]), str(res[1])]
+                                for z in range(0, len(array)):
+                                    worksheet.write(i, z, array[z])
+                    workbook.close()
+            except:
+                pass
+            finally:
+                con.close()
+        await ctx.message.author.send("Voici ton export en csv 🙂 :", file=discord.File(name + ".csv"))
+        await ctx.message.author.send("Et en version xlsx 🙂 :", file=discord.File(name + ".xlsx"))
+        os.remove(name + ".csv")
+        os.remove(name + ".xlsx")
+    else:
+        await ctx.message.channel.send("⛔ Tu n'as pas la permission de faire ceci ! ⛔")
+
+@client.command(name='switchall')
+@commands.guild_only()
+async def _switchAll(ctx, old_Role: discord.Role, new_Role: discord.Role, keep="false"):
+    x = 0
+    print("{0} used switchall cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976 or ("staff epitech" in [z.name.lower() for z in ctx.message.author.roles])):
+        async for member in ctx.message.guild.fetch_members(limit=None):
+            if (old_Role.name.lower() in [y.name.lower() for y in member.roles]):
+                x += 1
+                await member.add_roles(new_Role)
+                if (keep == "false"):
+                    await member.remove_roles(old_Role)
+        if (keep == "false"):
+            msg = { "Your request has been processed :" : "All '" + old_Role.name + "' members were moved to '" + new_Role.name + "' role !\n" + str(x) + " members were moved."}
+        else:
+            msg = { "Your request has been processed :" : "All '" + old_Role.name + "' members were granted '" + new_Role.name + "' role !\n" + str(x) + " members got this new role."}
+        await ctx.message.channel.send(embed=make_embed(title="Success !", nb_field=len(msg), fields=msg, inline=False, color=2))
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+@client.command(name='collect')
+@commands.guild_only()
+async def _collect(ctx, id: int):
+    print("{0} used collect cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976 or ("staff epitech" in [z.name.lower() for z in ctx.message.author.roles])):
+        target = ctx.message.guild.get_member(id)
+        if (target == None):
+            msg = { "An error occured :" : "Unable to find this member !"}
+            await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+        else:
+            msg = { "Your demand is processing ..." : f"Start collecting data of {target.display_name}."}
+            await ctx.message.channel.send(embed=make_embed(title="Success !", nb_field=len(msg), fields=msg, inline=False, color=2))
+            subprocess.Popen(["python3", "collector.py", str(target.id)], shell=False)
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+@client.command(name='collectors')
+async def _collectors(ctx):
+    print("{0} used collectors cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976):
+        con = create_con()
+        try:
+            with con.cursor() as c:
+                c.execute("SELECT step,uid,insert_date,comment,pid_collector FROM EpiCom.members WHERE step != 2;")
+                res = c.fetchall()
+            msg = "{:^20}{:^20}{:^40}{:^25}{:^6}\n".format("Status","UID","Date","Comment", "PID")
+            for x in res:
+                msg += "{:^20}{:^20}{:^40}{:^25}{:^6}\n".format(str(x[0]), str(x[1]), str(x[2]), str(x[3]), str(x[4]))
+            await ctx.message.channel.send("```" + msg + "```")
+        except:
+            msg = { "An error occured :" : "Unable to get list of collectors in database !"}
+            await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+        finally:
+            con.close()
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+@client.command(name="kill")
+async def _kill(ctx, pid : int):
+    print("{0} used kill cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976):
+        con = create_con()
+        try:
+            with con.cursor() as c:
+                c.execute("SELECT pid_collector FROM EpiCom.members WHERE (pid_collector = %s);", (pid))
+                if (c.fetchone() != None):
+                    os.kill(self.proc.pid, signal.SIGINT)
+                    c.execute("UPDATE EpiCom.members SET pid_collector WHERE (pid_collector = %s);", (pid))
+                    con.commit()
+                else:
+                    raise Exception("Lol")
+            msg = { "Your demand has been processed !" : f"Collector pid {str(pid)} was terminated."}
+            await ctx.message.channel.send(embed=make_embed(title="Success !", nb_field=len(msg), fields=msg, inline=False, color=2))
+        except:
+            msg = { "An error occured :" : "Unable to terminate this !"}
+            await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+        finally:
+            con.close()
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+
+@client.command(name="info")
+@commands.guild_only()
+async def _info(ctx, member):
+    print("{0} used info cmd !".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976 or ("staff epitech" in [z.name.lower() for z in ctx.message.author.roles])):
+        if (member.isnumeric()):
+            member = ctx.message.guild.get_member(int(member))
+            print(member)
+        con = create_con()
+        try:
+            with con.cursor() as c:
+                c.execute("SELECT fullname,studies,comment,insert_date FROM EpiCom.members WHERE (uid = %s);", (str(member.id)))
+                res = c.fetchone()
+            if (res == None):
+                raise ValueError("Lol")
+            embed = discord.Embed(colour=discord.Colour(0x3700ff))
+            embed.set_thumbnail(url=f"{member.avatar_url}")
+            embed.set_author(name="Informations about :", icon_url="https://i.ibb.co/sqfPRNC/info.png")
+            embed.set_footer(text="Dewey bot, by Maxime D.", icon_url="https://cdn.discordapp.com/icons/691661291785551873/f5fd1028da30dc55eb06ea77f6dbf222.png?size=128")
+            embed.add_field(name= (f"{member.name}#{member.discriminator}"), value=f"UID - {member.id}\nName - {res[0]}\nStudies - {res[1]}\nComment - {res[2]}\nDate join - {res[3]}\n")
+            await ctx.message.author.send(embed=embed)
+        except:
+            msg = { "An error occured :" : "This user does not exists in our records !"}
+            await ctx.message.author.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+        finally:
+            con.close()
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+@client.command(name="send")
+@commands.guild_only()
+async def _send(ctx, role : str, message : str):
+    print("{0} used send cmd !".format(ctx.message.author))
+    sended = 0
+    if (ctx.message.author.id == 277461601643134976 or ("staff epitech" in [z.name.lower() for z in ctx.message.author.roles])):
+        async for x in ctx.message.guild.fetch_members(limit=None):
+            if (role.lower() in [y.name.lower() for y in x.roles]):
+                try:
+                    await x.send(message)
+                    sended += 1
+                except:
+                    pass
+        msg = { "Your demand has been processed !" : f"Your message has been sent to {str(sended)} members !"}
+        await ctx.message.channel.send(embed=make_embed(title="Success !", nb_field=len(msg), fields=msg, inline=False, color=2))
+    else:
+        msg = { "An error occured :" : "You don't have permission to do that !"}
+        await ctx.message.channel.send(embed=make_embed(title="Something went wrong !", nb_field=len(msg), fields=msg, inline=False, color=0))
+
+@client.command(name="setwelcome")
+async def _setWelcome(ctx, channel : int):
+    print("{0} used setwelcome".format(ctx.message.author))
+    if (ctx.message.author.id == 277461601643134976):
+        welcome_channel = channel
+        with open("welcome.txt", "w") as f:
+            f.write(str(channel))
+        print(client.get_channel(welcome_channel))
+        msg = { "Your demand has been processed !" : f"Welcome messages will now be sent to {str(client.get_channel(welcome_channel))} !"}
+        await ctx.message.channel.send(embed=make_embed(title="Success !", nb_field=len(msg), fields=msg, inline=False, color=2))
+        
+
+def read_token():
+    with open("token.txt", "r") as f:
+        return (f.readlines()[0].strip())
+client.run(read_token())
